44f22bb875bb692e9e2e74adb1379a4cafa85e1a
Piyush Patel <piyushpatel2005@gmail.com>
2024-06-02 13:14:01 -0400

    added Dataframe introduction code

--- src/main/scala/section1/dataframes/DataFrameIntro.scala
+++ src/main/scala/section1/dataframes/DataFrameIntro.scala
@@ -1,6 +1,7 @@
 package section1.dataframes
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types._
 
 object DataFrameIntro extends App {
     // 1. Create spark session
@@ -14,7 +15,7 @@ object DataFrameIntro extends App {
     val df = spark.read
         .option("header", "true")
         .option("inferSchema", "true")
-        .csv("src/main/resources/data/retail-data/by-day/2010-12-01.csv")
+        .csv("src/main/resources/data/retail/sales_dataset.csv")
 
     // 3. Show the dataframe
     df.show()
@@ -26,5 +27,57 @@ object DataFrameIntro extends App {
     df.show(2)
 
     // 6. Take 2 rows
-    df.take(2).foreach(println)
+    df.take(5).foreach(println)
+
+    // 7. Count rows
+    val count = df.count()
+    println(s"Count of rows: $count")
+
+    // 8. Defining schema
+    val salesSchema = StructType(
+        Array(
+            StructField("Transaction_ID", StringType),
+            StructField("Date", StringType),
+            StructField("Customer_ID", StringType),
+            StructField("Gender", StringType),
+            StructField("Age", IntegerType),
+            StructField("Product_Category", StringType),
+            StructField("Quantity", IntegerType),
+            StructField("Price_per_Unit", DoubleType),
+            StructField("Total_Amount", DoubleType)
+        )
+    )
+
+    // 9. Using schema with format method
+    val dfWithSchema = spark.read
+        .format("csv")
+        .schema(salesSchema)
+        .option("inferSchema", "false")
+        .option("header", "true")
+        .load("src/main/resources/data/retail/sales_dataset.csv")
+
+    dfWithSchema.show(5)
+
+    dfWithSchema.printSchema()
+
+    /**
+     * Next read file sales_dataset_with_null_and_quotes.csv.
+     * This file has couple of null values marked as "NA"
+     * It also has quotes around string fields with character single quote (').
+     * It has delimiter of colon (:)
+     * It does not have header
+     */
+
+    val df2 = spark.read
+        .schema(salesSchema)
+        .option("header", "false")
+        .option("inferSchema", "false")
+        .option("sep", ":")
+        .option("nullValue", "NA")
+        .option("quote", "'")
+        .csv("src/main/resources/data/retail/sales_dataset_with_null_and_quotes.csv")
+
+    df2.show()
+
+    // More options here: https://spark.apache.org/docs/latest/sql-data-sources-csv.html
 }
